e869a9561da6d2ac737da78eb395881ec5221905
Pablo Olle <156226950+impoldev@users.noreply.github.com>
2025-08-27 13:24:36 -0300

    message

--- app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello asdasdasdsd $name!",
+        text = "Hello asdasdasdsasdasdd $name!",
         modifier = modifier
     )
 }
