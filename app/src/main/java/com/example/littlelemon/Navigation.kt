d68db88f5468663c1209c811fd001bf58f53609e
Pablo Olle <156226950+impoldev@users.noreply.github.com>
2025-08-28 19:16:13 -0300

    add destinations

---
+++ app/src/main/java/com/example/littlelemon/Navigation.kt
@@ -0,0 +1,38 @@
+package com.example.littlelemon
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    val context = LocalContext.current
+
+    val startDestination = remember {
+        val sharedPrefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+        val first = sharedPrefs.getString("first", "")
+        val last = sharedPrefs.getString("last", "")
+        val email = sharedPrefs.getString("email", "")
+        if (first != "" && last != "" && email != "") Home.route else Onboarding.route
+    }
+
+    NavHost(
+        navController = navController,
+        startDestination = startDestination
+    ) {
+        composable(Home.route) {
+            Home(navController = navController)
+        }
+        composable(Profile.route) {
+            Profile(navController = navController)
+        }
+        composable(Onboarding.route) {
+            Onboarding(navController = navController)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/littlelemon/Profile.kt
@@ -0,0 +1,162 @@
+package com.example.littlelemon
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import androidx.core.content.edit
+
+@Composable
+fun Profile(navController: NavController) {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+
+            Row(
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.logo),
+                    contentDescription = "Little Lemon logo",
+                    modifier = Modifier
+                        .width(260.dp)
+                        .height(80.dp)
+                        .padding(horizontal = 20.dp)
+                )
+            }
+
+            val context = LocalContext.current
+            val sharedPrefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+            val first = sharedPrefs.getString("first", "") ?: ""
+            val last = sharedPrefs.getString("last", "") ?: ""
+            val email = sharedPrefs.getString("email", "") ?: ""
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp)
+            ) {
+                Text(
+                    text = "Profile information:",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .padding(vertical = 40.dp)
+
+                )
+
+                Column (modifier = Modifier.padding(bottom = 20.dp)) {
+                    Text(text = "First name", modifier = Modifier.padding(bottom = 5.dp))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .border(
+                                width = 1.dp,
+                                color = Color.Black,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .padding(15.dp)
+                    ) {
+                        Text(text = first)
+                    }
+                }
+
+                Column (modifier = Modifier.padding(bottom = 20.dp)) {
+                    Text(text = "Last name", modifier = Modifier.padding(bottom = 5.dp))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .border(
+                                width = 1.dp,
+                                color = Color.Black,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .padding(15.dp)
+                    ) {
+                        Text(text = last)
+                    }
+                }
+
+                Column (modifier = Modifier.padding(bottom = 20.dp)) {
+                    Text(text = "Email", modifier = Modifier.padding(bottom = 5.dp))
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .border(
+                                width = 1.dp,
+                                color = Color.Black,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .padding(15.dp)
+                    ) {
+                        Text(text = email)
+                    }
+                }
+
+
+            }
+
+            Button(
+                onClick = {
+                    sharedPrefs.edit { clear() }
+                    navController.navigate(Onboarding.route)
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFF4CE14)
+                ),
+                shape = RoundedCornerShape(20),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 16.dp, end = 16.dp, bottom = 16.dp)
+            ) {
+                Text(text = "Logout", color = Color(0xFF485E57))
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ProfilePreview() {
+    val navController = rememberNavController()
+    Profile(navController)
+}
